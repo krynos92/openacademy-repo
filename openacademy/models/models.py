eb2d44b786e63414ca368606d647bb6cd009592c
Sebastian Peñaloza <krynos92@gmail.com>
2020-09-07 11:57:35 -0600

    [REF] constraints

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -46,7 +46,7 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    taken_seats = fields.Float(compute='_taken_seats')
+    taken_seats = fields.Float(compute='_taken_seats', store=True)
     active = fields.Boolean(default=True)
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
